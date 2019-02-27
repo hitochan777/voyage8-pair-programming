1c22f46a59d81fb5bee1b895f1f2c358e44756b6
Hitoshi Otsuki <hitochan777@gmail.com>
2019-02-27 11:42:07 +0000

    finish substring

---
+++ substring.js
@@ -0,0 +1,34 @@
+function searchForSubstring(stringToSearch, stringToFind) {
+  if (stringToFind.length === 0) {
+      return -1
+  }
+
+  for (let i = 0; i < stringToSearch.length; ++i) {
+      let isMatch = true
+      for (let j = 0; j < stringToFind.length; ++j) {
+          if (stringToSearch[i+j] !== stringToFind[j]) {
+              isMatch = false
+              break
+          }
+      }
+      if (isMatch) {
+          return i
+      }
+  }
+  return -1;
+}
+
+/*
+                    i
+                   012345
+stringToSearch     abbaba
+                    j
+                   01
+stringToFind       bb
+*/
+
+console.log(searchForSubstring("aababb", "bb") === 4)
+console.log(searchForSubstring("abcdef", "fdasfdas") === -1)
+console.log(searchForSubstring("abcdef", "cde") === 2)
+console.log(searchForSubstring("abcdef", "abcdef") === 0)
+console.log(searchForSubstring("abcdef", "") === -1)
